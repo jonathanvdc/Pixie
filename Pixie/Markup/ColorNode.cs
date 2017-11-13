cc0f94fff9c64089cbdc4f3d5d92ca41c82f6706
jonathanvdc <jonathan.vdc@outlook.com>
2017-11-13 15:48:48 +0100

    Define a ColorNode class

---
+++ Pixie/Markup/ColorNode.cs
@@ -0,0 +1,52 @@
+namespace Pixie.Markup
+{
+    /// <summary>
+    /// A markup node that specifies the color with which its contents
+    /// are printed.
+    /// </summary>
+    public sealed class ColorNode : ContainerNode
+    {
+        /// <summary>
+        /// Creates a color node from the given contents, foreground color
+        /// and background color.
+        /// </summary>
+        /// <param name="contents">The contents to print.</param>
+        /// <param name="foregroundColor">The foreground color to print the contents with.</param>
+        /// <param name="backgroundColor">The background color to print the contents with.</param>
+        public ColorNode(
+            MarkupNode contents,
+            Color foregroundColor,
+            Color backgroundColor)
+            : base(contents)
+        {
+            this.ForegroundColor = foregroundColor;
+            this.BackgroundColor = backgroundColor;
+        }
+
+        /// <summary>
+        /// Creates a color node from the given contents and foreground color.
+        /// </summary>
+        /// <param name="contents">The contents to print.</param>
+        /// <param name="foregroundColor">The foreground color to print the contents with.</param>
+        public ColorNode(
+            MarkupNode contents,
+            Color foregroundColor)
+            : this(contents, foregroundColor, Colors.Transparent)
+        { }
+
+        /// <summary>
+        /// Gets the foreground color that is applied to this node's contents.
+        /// </summary>
+        /// <returns>The foreground color.</returns>
+        public Color ForegroundColor { get; private set; }
+
+        /// <summary>
+        /// Gets the background color that is applied to this node's contents.
+        /// </summary>
+        /// <returns>The background color.</returns>
+        public Color BackgroundColor { get; private set; }
+
+        /// <inheritdoc/>
+        public override MarkupNode Fallback => Contents;
+    }
+}
