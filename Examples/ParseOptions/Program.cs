e5293db87512b6ba4199330da4323150c2f7aa49
jonathanvdc <jonathan.vdc@outlook.com>
2017-11-26 19:33:45 +0100

    Create an option parsing example

---
+++ Examples/ParseOptions/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Linq;
+using Pixie;
+using Pixie.Markup;
+using Pixie.Options;
+using Pixie.Terminal;
+
+namespace ParseOptions
+{
+    public static class Program
+    {
+        private static readonly FlagOption syntaxOnlyFlag = new FlagOption(
+            OptionForm.Short("fsyntax-only"),
+            OptionForm.Short("fno-syntax-only"),
+            false);
+
+        private static OptionSet parsedOptions;
+
+        public static void Main(string[] args)
+        {
+            // First, acquire a terminal log. You should acquire
+            // a log once and then re-use it in your application.
+            var log = TerminalLog.Acquire();
+
+            var allOptions = new Option[]
+            {
+                syntaxOnlyFlag
+            };
+
+            var parser = new GnuOptionSetParser(
+                allOptions, syntaxOnlyFlag, syntaxOnlyFlag.PositiveForms[0]);
+
+            parsedOptions = parser.Parse(args, log);
+
+            foreach (var item in allOptions)
+            {
+                log.Log(
+                    new LogEntry(
+                        Severity.Info,
+                        new BulletedList(
+                            allOptions
+                                .Select<Option, MarkupNode>(TypesetParsedOption)
+                                .ToArray<MarkupNode>(),
+                            true)));
+            }
+        }
+
+        private static MarkupNode TypesetParsedOption(Option opt)
+        {
+            return new Sequence(
+                new DecorationSpan(new Text(opt.Forms[0].ToString()), TextDecoration.Bold),
+                new Text(": "),
+                new Text(parsedOptions.GetValue<object>(opt).ToString()));
+        }
+    }
+}
