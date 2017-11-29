03d90be03e73b203dc6858c9f6c862fcada732f2
jonathanvdc <jonathan.vdc@outlook.com>
2017-11-29 07:43:52 +0100

    Define Box markup nodes, define BoxRenderer

--- Pixie.Terminal/Render/ParagraphRenderer.cs
+++ Pixie.Terminal/Render/ParagraphRenderer.cs
@@ -30,4 +30,32 @@ namespace Pixie.Terminal.Render
             state.Terminal.WriteSeparator(2);
         }
     }
+
+    /// <summary>
+    /// A renderer for box nodes.
+    /// </summary>
+    public sealed class BoxRenderer : NodeRenderer
+    {
+        private BoxRenderer() { }
+
+        /// <summary>
+        /// An instance of a box node renderer.
+        /// </summary>
+        public static readonly BoxRenderer Instance = new BoxRenderer();
+
+        /// <inheritdoc/>
+        public override bool CanRender(MarkupNode node)
+        {
+            return node is Box;
+        }
+
+        /// <inheritdoc/>
+        public override void Render(MarkupNode node, RenderState state)
+        {
+            var para = (Box)node;
+            state.Terminal.WriteSeparator(1);
+            state.Render(para.Contents);
+            state.Terminal.WriteSeparator(1);
+        }
+    }
 }
--- Pixie/Markup/Paragraph.cs
+++ Pixie/Markup/Paragraph.cs
@@ -23,4 +23,30 @@ namespace Pixie.Markup
             return new Paragraph(newContents);
         }
     }
+
+    /// <summary>
+    /// A markup node that visually separates its body from
+    /// other markup nodes by putting its body on different
+    /// lines from the box' successors and predecessors.
+    /// </summary>
+    public sealed class Box : ContainerNode
+    {
+        /// <summary>
+        /// Creates a box from a body node.
+        /// </summary>
+        /// <param name="contents">A node to insulate.</param>
+        public Box(MarkupNode contents)
+            : base(contents)
+        { }
+
+        /// <inheritdoc/>
+        public override MarkupNode Fallback =>
+            new Sequence(NewLine.Instance, Contents, NewLine.Instance);
+
+        /// <inheritdoc/>
+        public override ContainerNode WithContents(MarkupNode newContents)
+        {
+            return new Box(newContents);
+        }
+    }
 }
