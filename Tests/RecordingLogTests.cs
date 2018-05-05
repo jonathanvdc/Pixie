9f1f6dcac54cc3f312987055824dca9ebd3630d7
jonathanvdc <jonathan.vdc@outlook.com>
2018-05-05 16:25:45 +0200

    Test 'RecordingLog.Contains'

--- Tests/RecordingLogTests.cs
+++ Tests/RecordingLogTests.cs
@@ -18,6 +18,10 @@ namespace Pixie.Tests
             Assert.AreEqual(log.RecordedEntries.Count, 2);
             Assert.AreEqual(log.RecordedEntries[0].Severity, Severity.Message);
             Assert.AreEqual(log.RecordedEntries[1].Severity, Severity.Error);
+            Assert.IsTrue(log.Contains(Severity.Error));
+            Assert.IsFalse(log.Contains(Severity.Warning));
+            Assert.IsTrue(log.Contains(Severity.Message));
+            Assert.IsFalse(log.Contains(Severity.Info));
         }
 
         [Test]
@@ -36,11 +40,19 @@ namespace Pixie.Tests
             Assert.AreEqual(log2.RecordedEntries.Count, 2);
             Assert.AreEqual(log2.RecordedEntries[0].Severity, Severity.Message);
             Assert.AreEqual(log2.RecordedEntries[1].Severity, Severity.Warning);
+            Assert.IsFalse(log2.Contains(Severity.Error));
+            Assert.IsTrue(log2.Contains(Severity.Warning));
+            Assert.IsTrue(log2.Contains(Severity.Message));
+            Assert.IsFalse(log2.Contains(Severity.Info));
 
             Assert.AreEqual(log1.RecordedEntries.Count, 3);
             Assert.AreEqual(log1.RecordedEntries[0].Severity, Severity.Message);
             Assert.AreEqual(log1.RecordedEntries[1].Severity, Severity.Error);
             Assert.AreEqual(log1.RecordedEntries[2].Severity, Severity.Warning);
+            Assert.IsTrue(log1.Contains(Severity.Error));
+            Assert.IsTrue(log1.Contains(Severity.Warning));
+            Assert.IsTrue(log1.Contains(Severity.Message));
+            Assert.IsFalse(log1.Contains(Severity.Info));
         }
     }
 }
